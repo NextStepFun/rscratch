3c37b25875274c4aa21276c96f9f43f556141309
Enrico Rubboli <rubboli@gmail.com>
2014-04-06 00:04:07 +0200

    permetti registrazione di piu eventi per tasto

--- lib/game_window.rb
+++ lib/game_window.rb
@@ -1,5 +1,6 @@
 module RScratch 
   class GameWindow < Gosu::Window
+
     def initialize(width,height,caption)
       super width,height,false
       self.caption = caption
@@ -16,12 +17,13 @@ module RScratch
     end
 
     def register_key key, &block
-      @registered_keys[key] = block
+      @registered_keys[key] ||= []
+      @registered_keys[key].push block
     end
 
     def update
-      @registered_keys.each do |key,block| 
-        block.call if button_down? gosu_key(key)
+      @registered_keys.each do |key,blocks|
+        blocks.map(&:call) if button_down? gosu_key(key)
       end
     end
   end
