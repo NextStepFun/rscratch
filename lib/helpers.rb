89efbb8b0c54bae1255b5f4af5b22d473e115cdd
Enrico Rubboli <rubboli@gmail.com>
2014-04-06 02:35:20 +0200

    use RMagick for all kind of images

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,23 +1,19 @@
 require 'RMagick'
 module RScratch
   def get_image(image_name, opts={})
-    img = nil
+    image = nil
     ['png','jpg','jpeg','svg'].each do |ext|
       full_name = RSCRATCH_BASEDIR + "/media/sprites/#{image_name}.#{ext}"
       if File.exists?(full_name)
-        img = if ext=='svg'
-          svg = Magick::Image.read(full_name) do
-            self.background_color = "transparent"
-          end.first
-          svg.flop! if opts[:flip] == :horizontal
-          svg.flip! if opts[:flip] == :vertical
-          Gosu::Image.new($window, svg, false)
-        else
-          Gosu::Image.new($window, full_name, false)
-        end
+        img = Magick::Image.read(full_name) do
+          self.background_color = "transparent"
+        end.first
+        img.flop! if opts[:flip] == :horizontal
+        img.flip! if opts[:flip] == :vertical
+        image = Gosu::Image.new($window, img, false)
       end
     end
-    throw "immagine #{image_name} non trovata!" unless img
-    img
+    throw "immagine #{image_name} non trovata!" unless image
+    image
   end
 end
